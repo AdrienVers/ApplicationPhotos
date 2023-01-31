345a118dbb0d364f214d0eea69d8b3b4a0edcf29
AdrienVers <adrienvx77@outlook.fr>
2023-01-31 11:51:30 +0100

    fix png problem

--- src/components/AllPages/Navbar.tsx
+++ src/components/AllPages/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
 import Image from "next/image";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/Logo.png";
 import Link from "next/link";
 import { NAVBAR_LINKS } from "./NavbarLinks";
 
